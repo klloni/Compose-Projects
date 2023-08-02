ba6ad28bca94b1ede88e89a71389c4a46bbd4440
KL Loni <kiran.loni3@gmail.com>
2023-08-02 17:39:07 +0530

    viewmodel demo 2 completed, did changes in same project

--- app/src/main/java/com/klloni/viewmodeldemo1/MainActivity.kt
+++ app/src/main/java/com/klloni/viewmodeldemo1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.klloni.viewmodeldemo1
 
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -18,19 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         binding.textValue.text = viewModel.getValue().toString()
 
+
         binding.button.setOnClickListener {
-            viewModel.setValue(binding.etValue.text.toString().toInt())
+//            viewModel.setValue(binding.etValue.text.toString().toIntOrNull() ?: 0)
+            val inputText = binding.etValue.text.toString()
+            if (inputText.isNotBlank()) {
+                viewModel.setValue(inputText.toInt())
+            } else {
+                Toast.makeText(this, "Enter value..", Toast.LENGTH_SHORT).show()
+                viewModel.setValue(0)
+            }
             binding.textValue.text = viewModel.getValue().toString()
             binding.etValue.text.clear()
             val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
-
-
-        /*binding.countText.text = viewModel.getCurrentCount().toString()
-        binding.button.setOnClickListener {
-            viewModel.getUpdatedCount()
-            binding.countText.text = viewModel.getCurrentCount().toString()
-        }*/
     }
 }
