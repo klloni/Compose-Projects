dedd02711dab1ea694c23ae1a1e5df418d676da5
KL Loni <kiran.loni3@gmail.com>
2023-08-02 18:07:54 +0530

    Added viewmodelfactory to the project

--- app/src/main/java/com/klloni/viewmodeldemo1/MainActivity.kt
+++ app/src/main/java/com/klloni/viewmodeldemo1/MainActivity.kt
@@ -12,10 +12,12 @@ import com.klloni.viewmodeldemo1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        viewModelFactory = MainActivityViewModelFactory(125)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
 
         binding.textValue.text = viewModel.getValue().toString()
 
--- app/src/main/java/com/klloni/viewmodeldemo1/MainActivityViewModel.kt
+++ app/src/main/java/com/klloni/viewmodeldemo1/MainActivityViewModel.kt
@@ -2,9 +2,13 @@ package com.klloni.viewmodeldemo1
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(startingTotal: Int) : ViewModel() {
 
     private var total = 0
+
+    init {
+        total = startingTotal
+    }
 
     fun getValue(): Int {
         return total
